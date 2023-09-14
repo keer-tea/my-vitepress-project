5421c2dcace962d949b69dd10d54dd776c955aa2
lukecheng <lukecheng233@163.com>
2023-09-15 00:58:11 +0800

    add

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   title: '--VitePress--',
   description: 'Just playing around.',
   srcDir: 'src',
-  base: '/vitepress-course/',
+  base: '/my-vitepress-project/',
   head: [
     [
       // 配置浏览器标签页小图标
